61b54370de1af8bdfe08fdb2e994e29858446c43
AnthonyMejia0 <mejia.anthony96@gmail.com>
2021-08-22 04:49:24 -0700

    started intro

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -3,7 +3,12 @@ import './intro.scss'
 function Intro() {
     return (
         <div className="intro" id="intro">
-            
+            <div className="left">
+                <div className="imgContainer">
+                    <img src="assets/Selfie.jpg" alt="This is me." />
+                </div>
+            </div>
+            <div className="right"></div>
         </div>
     )
 }
