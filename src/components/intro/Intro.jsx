b0299dc62b7d50795c70d8ccda199a71cc9fe592
AnthonyMejia0 <mejia.anthony96@gmail.com>
2021-08-27 00:24:38 -0700

    made intro page mobile friendly

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,19 +1,24 @@
 import './intro.scss'
-import BG from './laptop.jpg'
 
 function Intro() {
     return (
         <div className="intro" id="intro">
-            <div className="imgContainer">
-                <img src={BG} alt="laptop" />
-            </div>
             <div className="text-box">
                 <h2>Hi, <br /> my name is <br /></h2>
-                <h2 className="name">Anthony.</h2>
+                <h2 className="name">Anthony</h2>
+            </div>
+            <div className="about-me">
+                <ul>
+                    <li>Programmer</li>
+                    <li>Tech Enthusiast</li>
+                    <li>Budding Web Developer</li>
+                </ul>
             </div>
             <div className="skillset">
-                <h3>Click to see my skillset</h3>
-                <span>v</span>
+                <a href='#portfolio'>
+                    <h3>Click to see my skillset</h3>
+                    <h3>v</h3>
+                </a>
             </div>
         </div>
     )
