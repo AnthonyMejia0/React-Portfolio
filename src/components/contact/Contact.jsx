47700f821888381aaf8572e422c29a6ce987ee83
AnthonyMejia0 <mejia.anthony96@gmail.com>
2022-05-24 17:16:08 -0700

    chagned intro bg and linked attribution

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -4,6 +4,13 @@ function Contact() {
     return (
         <div className="contact" id="contact">
 
+        <footer>
+            <div className="bg-att">
+                <button className="bg-auth">
+                    <a href='https://www.freepik.com/vectors/polygon-shape'>Polygon shape vector created by freepik - www.freepik.com</a>
+                </button>
+            </div>
+        </footer>
         </div>
     )
 }
