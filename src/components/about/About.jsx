e3293309d077edf28fa69402465b4b54eef0af38
AnthonyMejia0 <mejia.anthony96@gmail.com>
2022-05-25 14:27:23 -0700

    added about placeholder

---
+++ src/components/about/About.jsx
@@ -0,0 +1,11 @@
+import './About.scss'
+
+function About() {
+    return (
+        <div className="about" id="about">
+            <div className='title'>About Me</div>
+        </div>
+    )
+}
+
+export default About
