0f3d21f4cb66aae30dd5d65b4fa3d397905c2582
AnthonyMejia0 <mejia.anthony96@gmail.com>
2022-05-31 03:18:53 -0700

    added work in progress message

--- src/components/works/Works.jsx
+++ src/components/works/Works.jsx
@@ -3,7 +3,7 @@ import "./works.scss"
 function Works() {
     return (
         <div className="works" id="works">
-            
+            <h1>Projects page in progress.</h1>
         </div>
     )
 }
