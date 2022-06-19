b8c0c60ef46af9c071591a451fed75135566f3c7
AnthonyMejia0 <mejia.anthony96@gmail.com>
2022-06-19 15:03:46 -0700

    swapped skill icon

--- src/components/skills/Skills.jsx
+++ src/components/skills/Skills.jsx
@@ -9,7 +9,7 @@ function Skills() {
         <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/python/python-original-wordmark.svg" alt='Python Icon'/>
         <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg" alt='HTML5 Icon'/>
         <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg" alt='CSS3 Icon'/>
-        <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/sass/sass-original.svg" alt='SASS Icon'/>
+        <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/tailwindcss/tailwindcss-plain.svg" alt='Tailwind Icon' />
         <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original-wordmark.svg" alt='React Icon'/>
         <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg" alt='Javascript Icon'/>
         <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/postgresql/postgresql-plain-wordmark.svg" alt='PostgreSQL Icon'/>
