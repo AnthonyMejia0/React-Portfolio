1fc5ac7d3cc109f980a9b43b16864eb8bd1d18c4
AnthonyMejia0 <mejia.anthony96@gmail.com>
2022-07-15 13:21:03 -0700

    added scroll to top

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,19 @@ import Intro from "./components/intro/Intro"
 import About from "./components/about/About"
 import Works from "./components/works/Works"
 import Contact from "./components/contact/Contact"
+import ScrollToTop from "react-scroll-to-top"
 import "./app.scss"
 
-
 function App() {
-
   return (
     <div className="App">
-      <div className="sections">
-        <Intro />
-        <About />
-        <Works />
-        <Contact />
+      <div className="scroll">
+        <ScrollToTop smooth style={{}} color="#4FB286"/>
       </div>
+      <Intro />
+      <About />
+      <Works />
+      <Contact />
     </div>
   );
 }
