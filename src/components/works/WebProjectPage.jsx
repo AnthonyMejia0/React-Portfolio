1add305509073c0ffcf5238357f43190f7307c24
AnthonyMejia0 <mejia.anthony96@gmail.com>
2022-07-18 13:16:23 -0700

    added snake

--- src/components/works/WebProjectPage.jsx
+++ src/components/works/WebProjectPage.jsx
@@ -2,6 +2,7 @@ import WebProject from "./WebProject";
 import "./webprojectpage.scss";
 import spotify from "./SpotifyHome.png";
 import todo from "./ToDoList.png";
+import snake from "./Snake.png"
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import { Carousel } from "react-responsive-carousel";
 
@@ -21,6 +22,13 @@ function WebProjectPage() {
                 desc="This is a To Do List built using React with hooks and Recoil for state management along with local storage."
                 img={todo}
                 url="https://todo-list-ecru-chi.vercel.app/"
+            />
+
+            <WebProject 
+              title="Snake Game"
+              desc="The classic snake game built using react with hooks."
+              img={snake}
+              url={"https://snake-game-steel.vercel.app/"}
             />
         </Carousel>
     </div>
